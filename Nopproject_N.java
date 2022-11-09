c5f8f4e9005db0a5fef291a2b9977eef971a50de
balazs955 <83276057+balazs955@users.noreply.github.com>
2022-11-09 12:58:37 +0100

    Add files via upload

---
+++ Nopproject_N.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nopproject_n;
+import java.util.Scanner;
+
+/**
+ *
+ * @author user3
+ */
+public class Nopproject_N {
+    public static boolean szev(int szokoev) {
+    if (szokoev % 4 != 0) {
+        return false;
+  } else if (szokoev % 400 == 0) {
+        return true;
+  } else if (szokoev % 100 == 0) {
+        return false;
+  } else {
+        return true;
+  }
+    }
+    public static int sorrend(int num) {
+      int reversed = 0;
+      for(;num != 0; num /= 10) {
+      int digit = num % 10;
+      reversed = reversed * 10 + digit;
+      }
+      return reversed;
+    }
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Kérek egy évszámot: ");
+        int s = myObj.nextInt();
+        System.out.println("Szökőév-e ? " + szev(s));
+        System.out.println("Az évszám fordított sorrendje: " + sorrend(s));
+    }
+
+    
+    }
+
+}
